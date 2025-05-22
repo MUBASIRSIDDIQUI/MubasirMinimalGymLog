bbd6315d691387740890feef1a4a9a0f0e4d7537
Mubasir.Siddiqui <Mubasir.Siddiqui@ril.com>
2025-05-22 13:55:28 +0530

    Initial Commit

---
+++ MubasirMinimalGymLogApp/MubasirMinimalGymLogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MubasirMinimalGymLogAppApp.swift
+//  MubasirMinimalGymLogApp
+//
+//  Created by Mubasir Siddiqui on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MubasirMinimalGymLogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
